1b8d3c0700dec02c5629a65cca978a7e9c1a058e
Emad <abbasianemad5@gmail.com>
2026-05-16 16:20:13 -0700

    Uploading file added

--- Platform/Bale.py
+++ Platform/Bale.py
@@ -1,11 +1,12 @@
 from balethon import Client
+import httpx, io
 from balethon.conditions import private
 from balethon.objects import InlineKeyboard
 import asyncio
-from dictation.dic_word import dictation , only_removing
+from dictation.dic_word import dictation , only_removing , find_similar_songs
 from dictation.ai_text import correct_grammar
 from decouple import config
-from web_scraping.upmusic import find_song,search_song
+from web_scraping.upmusic import process_search_query
 from spotify_service.Formatter import format_song
 from youtube_service.Search_System import search_youtube
 
@@ -61,9 +62,57 @@ async def get_song_name(*, message):
     if current_state == "waiting_for_name":
         removed_text = await only_removing(text)
         song = correct_grammar(removed_text)
-        await message.reply(
+        msg = await message.reply(
             f"🔍 در حال جستجوی:\n*{song}*")
-    # await bot.send_chat_action(chat_id)
+        await bot.send_chat_action(chat_id)
+        
+        results = process_search_query(song)
+        if results is None:
+            await msg.edit("آهنگی پیدا نشد، لطفا آهنگ مورد نظر تون رو وارد کنید")
+            print(results)
+        # print(results)
+        else:
+            music_info = await find_similar_songs(song, results)
+            print('music_info')
+            if not music_info:
+                await msg.edit("آهنگ مرتبطی پیدا نشد.")
+                return
+
+        file_url = music_info[1]
+        song_name = music_info[0]
+        print('file_url')
+        try:
+            print('before +++++ ersal mishe?')
+            async with httpx.AsyncClient(follow_redirects=True, timeout=60) as client:
+                response = await client.get(file_url)
+                response.raise_for_status()
+
+                file_content = response.content
+                in_memory_file = io.BytesIO(file_content)
+
+                file_name = file_url.split("/")[-1] or f"{song}.mp3"
+                in_memory_file.name = file_name  # خیلی مهم برای بعضی کتابخانه‌ها
+                in_memory_file.seek(0)
+                
+                await bot.send_audio(
+                    chat_id=chat_id,
+                    audio=in_memory_file,
+                    title = (song_name),
+                    caption=f"🎵 آهنگ: {song}",
+                    reply_to_message_id=msg.id
+                )
+                print('after ++++++ ersal mishe?')
+
+                await bot.delete_message(chat_id, msg.id)
+
+        except httpx.HTTPStatusError as e:
+            await msg.edit(f"خطا در دانلود فایل ({e.response.status_code}): {e.response.reason_phrase}")
+        except httpx.RequestError as e:
+            await msg.edit(f"خطای شبکه در هنگام دانلود: {e}")
+        except Exception as e:
+            await msg.edit(f"خطای غیرمنتظره در ارسال فایل: {e}")
+
+            
     # await message.reply(
     #     find_song(search_song(song)[0]))
     
@@ -76,6 +125,7 @@ async def get_song_name(*, message):
     elif current_state is None:
         await message.reply('از منوی استارت یک گزینه را انتخاب کنید \n/start'
         )
+
 
 @bot.on_command(private, name="help")
 async def help_command(*, message):
