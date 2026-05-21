84fd529a87ee8b3f34808ecd31b6d4f3831c93c3
Emad <abbasianemad5@gmail.com>
2026-05-21 04:09:08 -0700

    Adding two inline methods and some developing in sending audios and Exception Handling/ closing clients

--- Platform/Bale.py
+++ Platform/Bale.py
@@ -4,9 +4,11 @@ from balethon.conditions import private
 from balethon.objects import InlineKeyboard
 import asyncio
 from dictation.dic_word import dictation , only_removing , find_similar_songs
-from dictation.ai_text import correct_grammar
+# from dictation.ai_text import correct_grammar 
+from dictation.ai_singer_name import dedicate_singer
 from decouple import config
-from web_scraping.upmusic import process_search_query
+from web_scraping.upmusic import process_search_query, close_client
+from .audio_downloader import send_music, close_download_client
 from spotify_service.Formatter import format_song
 from youtube_service.Search_System import search_youtube
 
@@ -22,7 +24,9 @@ async def start(*, message):
     await message.reply(
         "*سلام من ربات جستوجوی اهنگ هستم\nاینجام تا اهنگ های تورو پیدا کنم*",
         InlineKeyboard(
-            [('جستوجو با اسم اهنگ و خواننده', 'waiting_for_name')],[('جستوجو با متن اهنگ', 'waiting_for_text')]
+            [('جستوجو با اسم اهنگ و خواننده', 'waiting_for_name')],[('جستوجو با متن اهنگ', 'waiting_for_text')],
+            [('جستوجو با ارسال وویس', 'waiting_for_voice')],
+            [('جستوجو با ارسال لینک (یوتیوب / اسپادیفای)', 'waiting_for_link')]
         )
     )
     
@@ -43,6 +47,16 @@ async def answer_callback_query(callback_query):
         await callback_query.message.edit('لطفا متن اهنگ مورد نظر را وارد کنید')
         await callback_query.answer('جستوجو با متن اهنگ')
 
+    elif callback_query.data == 'waiting_for_voice':
+        user_state[user_id]["state"] = "waiting_for_text"
+        await callback_query.message.edit('لطفا وویس اهنگ مورد نظر را ارسال کنید')
+        await callback_query.answer('جستوجو با وویس اهنگ')
+
+    elif callback_query.data == 'waiting_for_link':
+        user_state[user_id]["state"] = "waiting_for_text"
+        await callback_query.message.edit('لطفا لینک اهنگ مورد نظر را ارسال کنید')
+        await callback_query.answer('جستوجو با لینک اهنگ')
+        
     else:
         await callback_query.message.reply('از منوی استارت یک گزینه را انتخاب کنید \n/start'
         )
@@ -53,23 +67,28 @@ async def answer_callback_query(callback_query):
 @bot.on_message(private)
 async def get_song_name(*, message):
     user_id = message.author.id
+    user_mame = message.author.username
+    user_firstname = message.author.first_name
     chat_id = message.chat.id
     text = message.text
+    print(f"User ID : {user_id}   Message ID : {chat_id}")
 
     # Check if the user's state is "waiting_for_name"
     current_state = user_state.get(user_id, {}).get("state")
 
     if current_state == "waiting_for_name":
         removed_text = await only_removing(text)
-        song = correct_grammar(removed_text)
+        song =   removed_text
+        # song = correct_grammar(removed_text)
         msg = await message.reply(
             f"🔍 در حال جستجوی:\n*{song}*")
         await bot.send_chat_action(chat_id)
         
-        results = process_search_query(song)
+        results = await process_search_query(song)
         if results is None:
             await msg.edit("آهنگی پیدا نشد، لطفا آهنگ مورد نظر تون رو وارد کنید")
             print(results)
+            return
         # print(results)
         else:
             music_info = await find_similar_songs(song, results)
@@ -79,48 +98,53 @@ async def get_song_name(*, message):
                 return
 
         file_url = music_info[1]
-        song_name = music_info[0]
-        print('file_url')
+        names_song = dedicate_singer(music_info[0])
+        # print(type(names_song))
+        # print(names_song)
         try:
+
             print('before +++++ ersal mishe?')
-            async with httpx.AsyncClient(follow_redirects=True, timeout=60) as client:
-                response = await client.get(file_url)
-                response.raise_for_status()
+            # audio_file = await download_and_tag(file_url, names_song.get('song'), names_song.get('artis'))
 
-                file_content = response.content
-                in_memory_file = io.BytesIO(file_content)
+            await send_music(bot, chat_id, file_url, names_song.get('song'), names_song.get('artist'))
+            print('after ++++++ ersal mishe?')
 
-                file_name = file_url.split("/")[-1] or f"{song}.mp3"
-                in_memory_file.name = file_name  # خیلی مهم برای بعضی کتابخانه‌ها
-                in_memory_file.seek(0)
-                
-                await bot.send_audio(
-                    chat_id=chat_id,
-                    audio=in_memory_file,
-                    title = (song_name),
-                    caption=f"🎵 آهنگ: {song}",
-                    reply_to_message_id=msg.id
-                )
-                print('after ++++++ ersal mishe?')
-
-                await bot.delete_message(chat_id, msg.id)
+            await bot.delete_message(chat_id, msg.id)
 
         except httpx.HTTPStatusError as e:
-            await msg.edit(f"خطا در دانلود فایل ({e.response.status_code}): {e.response.reason_phrase}")
+            await msg.edit("خطا در دانلود فایل ")
+            await bot.send_message(
+            962702339,
+            f"خطا در دانلود فایل:\n{str(e)}\n\n"
+            f"از کاربر: {user_id}\n"
+            f"با نام: {user_firstname}\n"
+            f"نام کاربری: {user_mame}"
+        )
         except httpx.RequestError as e:
-            await msg.edit(f"خطای شبکه در هنگام دانلود: {e}")
+            await msg.edit("خطای شبکه در هنگام دانلود")
+            await bot.send_message(
+            962702339,
+            f"خطای شبکه در هنگام دانلود:\n{str(e)}\n\n"
+            f"از کاربر: {user_id}\n"
+            f"با نام: {user_firstname}\n"
+            f"نام کاربری: {user_mame}"
+        )
         except Exception as e:
-            await msg.edit(f"خطای غیرمنتظره در ارسال فایل: {e}")
-
+            await msg.edit("خطای غیرمنتظره در ارسال فایل")
+            await bot.send_message(
+            962702339,
+            f"خطای غیرمنتظره در ارسال فایل:\n{str(e)}\n\n"
+            f"از کاربر: {user_id}\n"
+            f"با نام: {user_firstname}\n"
+            f"نام کاربری: {user_mame}"
+        )
             
-    # await message.reply(
-    #     find_song(search_song(song)[0]))
     
     elif current_state == 'waiting_for_text':
         dictated_text = await dictation(text)
-        text_song = correct_grammar(dictated_text)
+        # text_song = correct_grammar(dictated_text)
         await message.reply(
-            f"🔍 در حال جستجوی متن آهنگ:\n*{text_song}*")
+            f"🔍 در حال جستجوی متن آهنگ:\n*{dictated_text}*")
         
     elif current_state is None:
         await message.reply('از منوی استارت یک گزینه را انتخاب کنید \n/start'
@@ -133,8 +157,13 @@ async def help_command(*, message):
 
 
 def bot_run():
-    print("Bot is running...")
-    bot.run()
+    try:
+        print("Bot is running...")
+        bot.run()
+    finally:
+        asyncio.run(close_client())
+        asyncio.run(close_download_client())
+    
 
 
 
